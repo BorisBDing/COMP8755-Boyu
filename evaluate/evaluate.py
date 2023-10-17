01e8066f7afe713917c4e209b8f6149ff2836bbe
BorisBDing <89572598+BorisBDing@users.noreply.github.com>
2023-10-17 20:26:20 +1100

    Update evaluate.py

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import math
 
+#clicking module and weight function
 def weight(index, row=0, column=0):
     row = index / 5
     column = index % 5
@@ -18,6 +19,7 @@ def clean():
             counter[dic][attr_name]["output"] = 0
             counter[dic][attr_name]["interface"] = 0
 
+#计算bias的方法
 def calculate_bias1(counter):
     inline_bias = math.sqrt(np.mean(np.square(counter["input"])))
     content_diff = counter["input"] - counter["output"]
@@ -39,6 +41,7 @@ def calculate_bias3(counter):
     return content_bias, interface_bias
 
 bias = {}
+#计算bias
 def get_bias(key):
     dics = []
     if key in counter.keys():
@@ -69,7 +72,7 @@ def get_bias(key):
 attributes = ["century","country","medium"]
 req_key = ["vase","bottle","bowl","chair","cup","plate","pot","table"]
 counter = {}
-
+#读取文件与数据
 csv_file="../db/data.csv"
 data_path = csv_file
 dir_path = os.path.dirname(data_path)
@@ -91,15 +94,16 @@ for obj_name in objs.index:
         counter[obj_name][attr_name]=attr
 res_item=objs
 res_item['count'] = 0
-
+#根据文件名与id设置图片路径
 for doc in docs:
     doc.uri = dir_path + "/"+str(doc.tags['category'])+"/" + str(doc.tags["objid"]) + ".jpg"
-
+#创建连接
 client = Client('http://0.0.0.0:51200')
 client._client.post(on='/clear')
 client.index(docs, show_progress=True, return_results=True)
 
 for key in req_key:
+    #搜索相关的key
     ret = client.search([key,],limit=50)
     res = ret['@m']
     bias[key] = {}
@@ -114,5 +118,5 @@ for key in req_key:
         index += 1
     print(key)
     get_bias(key)
-    print(res_item)
+    #print(res_item)
     #print(bias)
